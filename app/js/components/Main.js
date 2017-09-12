42a0292e6d52316cf0880f1faf7f9d2ba089aace
John Bendfeldt <jfbendfeldt@gmail.com>
2017-09-12 18:51:11 -0500

    fixed the header for now

--- app/js/components/Main.js
+++ app/js/components/Main.js
@@ -35,6 +35,7 @@ export default class Main extends React.Component{
                   <li><Link to="/home">Home</Link></li>
                   <li><Link to="/profile">Profile</Link></li>
                   <li><Link to="/login">Login</Link></li>
+                  <li><Link to="/signup">Signup</Link></li>
                 </ul>
                 <ul className="side-nav" id="mobile-demo">
                   <li><Link to="/home">Home</Link></li>
