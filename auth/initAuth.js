25170dacac3e1d3455bf86dd0affccabe246f1c8
John Bendfeldt <jfbendfeldt@gmail.com>
2017-09-14 21:21:12 -0500

    spelling

--- auth/initAuth.js
+++ auth/initAuth.js
@@ -1,3 +1,3 @@
-import AuthService from './AuthService';
+import AuthService from './authService';
 const auth = new AuthService('XASKjI4mAfsyRqTf9Rjp7HecVTqPDMy_', 'todayilearnt.auth0.com')
 export default auth
