5709570f8a47b3a9555410ac6ed585047538cd46
John Bendfeldt <jfbendfeldt@gmail.com>
2017-09-26 19:09:10 -0500

    that works better for the transition.

--- app/js/components/Main.js
+++ app/js/components/Main.js
@@ -67,14 +67,14 @@ export default class Main extends React.Component{
                 <Link to="/" className="brand-logo">LearnT</Link>
                 <Link to="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></Link>
                 <ul className="right hide-on-med-and-down">
-                  <li><Link to={{pathname:"/home", state:{user:this.state.user}}}>Home</Link></li>
+                  <li><Link to="/home">Home</Link></li>
                   <li><Link to="/profile">Profile</Link></li>
                   <li><Link to="/login">Login</Link></li>
                   <li><Link to="/signup">Signup</Link></li>
                 </ul>
                 <ul className="side-nav" id="mobile-demo">
-                  <li><Link to={{pathname:"/home", state:{user:this.state.user}}}>Home</Link></li>
-                  <li><Link to={{pathname:"/profile", state:{user:this.state.user}}}>Profile</Link></li>
+                  <li><Link to="/home">Home</Link></li>
+                  <li><Link to="/profile">Profile</Link></li>
                   <li><Link to="/login">Login</Link></li>
                   <li><Link to="/signup">Signup</Link></li>
                 </ul>
